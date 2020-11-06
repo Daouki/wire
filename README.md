23adc0341f59bac3edf73d5aedbbb09a4e899689
Damian Koski <daouki97@gmail.com>
2020-11-06 17:01:09 +0100

    Setup project files

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Wire Programming Language
+
+Wire is a general-purpose programming language that transpiles to C++.
+
+## License
+
+Wire is distributed under the terms of the MIT license. See [LICENSE](LICENSE)
+for details.
---
+++ WireC.Driver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WireC.Driver
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("WireC v0.1.0");
+        }
+    }
+}
